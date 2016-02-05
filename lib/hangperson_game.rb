924bdd51ce8f2f3346edeaafe9a3b3363bd4d9e1
Charles Lee <charleslee94@berkeley.edu>
2016-02-05 01:03:17 -0800

    FInished tests for hangman game

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,7 +10,84 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+class String
+  def is_upper?
+    !!self.match(/\p{Upper}/)
+  end
+
+  def is_lower?
+    !!self.match(/\p{Lower}/)
+    # or: !self.is_upper?
+  end
+end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
+  def guess char
+    if char == nil
+      throw ArgumentError
+    end
+    if char == '' 
+      throw ArgumentError
+    end
+    if !char.match(/^[[:alpha:]]$/)
+      throw ArgumentError
+    end
+    if @guesses.include? char or @guesses.include? char.downcase
+      return false
+    end
+    if @wrong_guesses.include? char or @wrong_guesses.include? char.downcase
+      return false
+    end
+    if @word.include? char
+        @guesses += char
+        if check_win_or_lose
+          return :win
+        end
+        return true
+      else
+        @wrong_guesses += char
+      end
+  end
+
+  def word_with_guesses
+    matched = ''
+    wordlist = @word.split('')
+    wordlist.each do |char|
+      if @guesses.include? char
+        matched += char
+      else
+        matched += '-'
+      end
+    end
+    return matched
+  end
+
+  def check_win_or_lose
+    wordlist = word.split('')
+    matched = ''
+    wordlist.each do |char|
+      if @guesses.include? char
+        matched += char
+      else
+        matched+= '-'
+      end
+    end
+    if !matched.include? '-'
+      return :win
+    else
+
+      end
+      return :play
+    end
+  end
+
 
   def self.get_random_word
     require 'uri'
