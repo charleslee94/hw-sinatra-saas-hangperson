a45dd30d6dacf2e8b2f344cb6f2bba072f46e152
Charles Lee <charleslee94@berkeley.edu>
2016-02-09 02:12:12 -0800

    stops users from cheating

--- app.rb
+++ app.rb
@@ -43,8 +43,6 @@ class HangpersonApp < Sinatra::Base
     unique = @game.guess(letter)
     if unique == false
       flash[:message] = "You have already used that letter."
-    elsif unique == ArgumentError
-      flash[:message] = "Invald guess."
     end
     redirect '/show'
     end
@@ -55,7 +53,6 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
     outcome = @game.check_win_or_lose
     case outcome
     when :win
@@ -70,11 +67,17 @@ class HangpersonApp < Sinatra::Base
   
   get '/win' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :win
+      redirect '/show'
+    end
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :lose
+      redirect '/show'
+    end
     erb :lose # You may change/remove this line
   end
   
